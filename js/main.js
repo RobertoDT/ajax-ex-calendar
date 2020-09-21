f31e6c6035b3b053bb5bb4ccd8523773d7e09dbc
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-09-21 13:18:17 +0200

    Prima versione calendario completo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
 $(document).ready(function(){
+  var data = moment("2018-01-01");
 
-  var data = "2018-01-01";
+  stampaCalendario(data);
+  stampaFestivita(data);
+
+  $(".prev").click(function(){
+
+    if(data.format("M") == 1){
+      alert("non puoi tornare nell'anno 2017");
+    } else {
+      data.subtract(1, "months");
+      stampaCalendario(data);
+      stampaFestivita(data);
+
+    }
+
+  })
+
+  $(".next").click(function(){
+
+    if(data.format("M") == 12){
+      alert("non puoi andare nell'anno 2019");
+    } else {
+      data.add(1, "months");
+      stampaCalendario(data);
+      stampaFestivita(data);
+
+    }
+
+  })
+
+});
+
+
+
+//stamperà il mese che gli passo come argomento
+function stampaCalendario(data){
+  //pulisco subito il contenuto per avitare che printa (appende) i mesi uno sotto l'altro
+  $("#days").html("");
 
   var dataMomento = moment(data);
+  $("h1").text(dataMomento.format("MMMM YYYY"));
 
   var source = $("#day-template").html();
   var template = Handlebars.compile(source);
@@ -22,13 +60,20 @@ $(document).ready(function(){
     $("#days").append(html);
 
   }
+}
+
+//stampa le festività del mese che gli passo come a
+function stampaFestivita(data){
+
+  var dataMomento = moment(data);
+  var mese = dataMomento.format("M") - 1;
 
   $.ajax(
     {
       "url": "https://flynn.boolean.careers/exercises/api/holidays",
       "data": {
         "year": 2018,
-        "month": 0
+        "month": mese
       },
       "method": "GET",
       "success": function (data, stato) {
@@ -40,8 +85,11 @@ $(document).ready(function(){
           var nomeFestivita = festivita[i].name;
           var dataFestivita = festivita[i].date;
 
-          $(".day[data-completa='"+dataFestivita+"']").addClass("holiday");
-          $(".day[data-completa='"+dataFestivita+"'] .holidayType").text(" - " + nomeFestivita);
+          var item = $(".day[data-completa='"+dataFestivita+"']");
+
+          item.addClass("holiday");
+          item.children(".holidayType").text(" - " + nomeFestivita);
+
         }
 
       },
@@ -50,10 +98,10 @@ $(document).ready(function(){
     }
     }
   );
-
-});
+}
 
 function addZero(giorno){
+
   if(giorno < 10){
     return "0" + giorno;
   }
